ff4e3273edd2836c78de8dbc58ed225ec407255a
norihito-yamazaki-frontierfield <n.yamazaki@frontierfield.co.jp>
2024-03-14 16:15:18 +0900

    add base path

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,29 @@ import express from 'express';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { container } from './inversify.config';
 
-const server = new InversifyExpressServer(container);
-
-server.setConfig((app) => {
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-});
-
-const app = server.build();
-
 // todo:move config
 const port = 3000;
-app.listen(port, () => {
+
+const configFn = (app: express.Application) => {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+};
+
+const errorConfigFn = (app: express.Application) => {
+    app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+};
+
+const callback = () => {
     console.log(`Server running on http://localhost:${port}`);
-});
+};
+
+let server = new InversifyExpressServer(container, null, { rootPath: "/api/v1" });
+
+server
+    .setConfig(configFn)
+    .setErrorConfig(errorConfigFn)
+    .build()
+    .listen(port, 'localhost', callback);
